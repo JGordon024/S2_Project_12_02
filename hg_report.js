12183ea4d4139da00134e6daaf8ee9dcdfdfce1c
Unknown <jgordon024@west-mec.org>
2019-03-05 16:22:15 -0700

    Final Commit
    Finished project, I added two elements to the HTML and pulled them into the script file using their tagname. I created a formula to calculate the average review. I created a table that used information from the arrays to display the rating, condition, the game, ID, etc. I created a few for loops, one of which was used for generating 3 customer reviews.

--- hg_report.js
+++ hg_report.js
@@ -11,23 +11,33 @@
    Filename: hg_report.js
 	
 */
-
-var gameReport =
-    "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='id' id='gameImg' /> <table><tr><th>Product ID</th><td>id</td></tr> <tr><th>" + itemPrice + "</th><td>price</td></tr> <tr><th>" + itemPlatform + "</th><td>platform</td></tr> <tr><th>" + itemESRB + "</th><td>esrb</td></tr> <tr><th>" + itemCondition + "</th><td>condition</td></tr> <tr><th>" + itemRelease + "</th><td>release</td></tr></table>" + itemSummary;
-
-document.getElementsByTagName("article").innerHTML = gameReport;
-
-var ratingsSum = 0;
-
-var ratingsCount = ratings.length;
-
-for (var i = 0; i = ratingsSum; ratings + ratingsSum) {
-    var ratingsAvg = ratingsSum / ratingsCount;
-    ratingsReport = "<h1> Customer Reviews </h1> <h2>" + ratingsAvg + "out of 5 stars(" + ratingsCount + "reviews) </h2>";
+// Creates a variable that creates an HTML format that plugs in arrays from another file. 
+var gameReport = "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg' /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
+// Brings in the first and only article element and assigns it to game report
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+// Gives the initial value of ratingSum 0
+var ratingSum = 0;
+// The ratingCount variable is being asigned the value of the length of the ratings
+var ratingCount = ratings.length;
+// Creates a loop that adds the value of the index of ratings to ratingSum
+for (var i = 0; i <= ratingCount; i++) {
+    ratingSum += ratings[i];
 }
+//the rating average variable is equal to the ratingSum divided by ratingCount
+var ratingAvg = ratingSum / ratingCount;
+// Creates a ratingReport that will be the foundating for the reviews
+var ratingReport = "<h1>Customer Reviews</h1><h2>" + ratingAvg + "out of 5 stars (" + ratingCount + " reviews)</h2>";
+// Creates a for loop that runs up to 3 times, leaving a total of 3 reviews
+for (var i = 0; i <= 2; i++) {
 
-for () {
+    ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr><tr><th>Rating</th><td>";
+    // A nested variale that brings in the star image that be used for rating
+    for (var j = 1; j <= ratings[i]; j++) {
+        ratingReport += "<img src='hg_star.png' />";
 
-    "div class=”review”> <h1>title</h1><table> <tr><th>By</th><td>author</td></tr> <tr><th>Review Date</th><td>date</td></tr><tr><th>Rating</th><td>"
-
+    }
+    //ratingReport is having the end of the elements added to it, which is the end of the HTML
+    ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
 }
+// ratingReport is having the first aside element brought in
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
